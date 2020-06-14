ffdc07d77a93b1405c862f36b169e5ffddec0c83
karim amzil <karimamzil19@gmail.com>
2020-06-14 21:20:30 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Client from './Client';
+import Counter from './Counter';
+import ReactDOM from 'react-dom';
+import ClientForm from './ClientForm';
+class App extends React.Component{
+  nouveauClient=''
+    state={
+      clients:[
+        {id:1,nom:"Lior Chamla"},
+        {id:1,nom:"Magali Pernin"},
+        {id:3,nom:"Joseph Durand"}],
+    }
+    
+    handleAdd=(client)=>{
+    const clients=[...this.state.clients];
+    clients.push(client);
+    this.setState({clients});
+    }
+    handleDelete=(id)=>{
+      const clients=[...this.state.clients];
+      const index=clients.findIndex(client=>client.id===id);
+      clients.splice(index,1);
+      this.setState({clients:clients});
+    }
+    
+    
+  render(){
+    const title="Liste des Clients"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}</h1>
+      <Counter/>
+      <ul>
+        {this.state.clients.map(client=>
+        <Client details={client} onDelete={this.handleDelete}/>)} 
+      </ul>
+     <ClientForm onClientAdd={this.handleAdd}/>
     </div>
   );
-}
-
+}}
 export default App;
